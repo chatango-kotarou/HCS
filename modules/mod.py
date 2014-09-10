6059c3e802952b730f6726aa39581d56186905f7
Michael Borgfield <yuri9911324@gmail.com>
2014-09-09 21:40:20 -0400

    fixed stupid mistakes

--- modules/mod.py
+++ modules/mod.py
@@ -38,5 +38,5 @@ def unbanUser(mgr, args):
 	room = args[0]
 	user = args[1]
 	target = args[2]
-        self.unbanUser(mgr, args)
+        self.unbanUser(room, user, target)
         mgr.callProtocal('unbanned:%s:%s:%s:%s' % (room, user, target, time))
